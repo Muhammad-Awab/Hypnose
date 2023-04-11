3c3cde54707d66dbdab700752b5f7cb11dccfeff
Muhammad-Awab <muhammadawab82002@gmail.com>
2023-04-11 19:19:43 +0500

    CHanges

--- Hypnos/Data/HypnosService.cs
+++ Hypnos/Data/HypnosService.cs
@@ -7,11 +7,6 @@ namespace Hypnos.Data
     {
         private static List<HypnosQuestion>? Quesions;
 
-        private static List<HypnosQuestion>? legQuesions;
-        private static List<HypnosQuestion>? TobaccoQuesions;
-        private static List<HypnosQuestion>? sleepnessQuesions;
-
-        private static List<HypnosQuestion>? newQuesions;
 
         static HypnosService()
         {
@@ -21,7 +16,7 @@ namespace Hypnos.Data
                 {
                     Qno=1,
                     Category="sa",
-                    Title="Tobacco",
+                    Title="Sleep_Apnea",
                     Question = "Do you snore loudly?",
                     Choices = new List<string> {"Yes", "No","Please Select"},
                     AnswerIndex = 1,
@@ -30,7 +25,7 @@ namespace Hypnos.Data
                    new HypnosQuestion
                 {
                     Qno=2,
-                    Category="sa",Title="Sleep Apnea",
+                    Category="sa",Title="Sleep_Apnea",
                     Question = "Do you often feel tired, sleepy or fatigued during the daytime?",
                     Choices = new List<string> {"Yes", "No","Please Select"},
                     AnswerIndex = 1,
@@ -39,199 +34,294 @@ namespace Hypnos.Data
                    new HypnosQuestion
                 {
                     Qno=3,
-                    Category="sa",Title="Sleep Apnea",
+                    Category="sa",Title="Sleep_Apnea",
                     Question = "Has anyone observed you stop breating in your seelp?",
                     Choices = new List<string> {"Yes", "No","Please Select"},
                     AnswerIndex = 1,
                     Score = 3
-                }
-
-            };
-
-            TobaccoQuesions = new List<HypnosQuestion>()
-            {
-                   new HypnosQuestion
+                },
+                  new HypnosQuestion
                 {
                     Qno=4,
-                    Category="sa",Title="Tobacco Cessation",
-                    Question = "Do you have (or are you being treated for) high blood pressure?",
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "Do you have (or are you being treated for) high blood pressure",
                     Choices = new List<string> {"Yes", "No","Please Select"},
                     AnswerIndex = 1,
                     Score = 3
                 },
-                   new HypnosQuestion
+                      new HypnosQuestion
                 {
                     Qno=5,
-                    Category="sa",Title="Tobacco Cessation",
-                    Question = "In the following situations, how likely are you to nap or fall aspleep?",
-                    Choices = new List<string> { "Never", "slight", "moderate", "high","Please Select"},
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "Sitting and reading ",
+                    Choices = new List<string> {"Never", "slight","moderate","high","Please Select"},
                     AnswerIndex = 1,
                     Score = 3
                 },
-                   new HypnosQuestion
+                          new HypnosQuestion
                 {
                     Qno=6,
-                    Category="sa",Title="Tobacco Cessation",
-                    Question = "Sitting and reading ?",
-                    Choices = new List<string> { "Never", "slight", "moderate", "high","Please Select"},
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "Sitting and watching TV",
+                    Choices = new List<string> {"Never", "slight","moderate","high","Please Select"},
                     AnswerIndex = 1,
                     Score = 3
                 },
-                  
-
-            };
-
-            sleepnessQuesions = new List<HypnosQuestion>()
-            {
-                 new HypnosQuestion
-
+                              new HypnosQuestion
                 {
                     Qno=7,
-                    Category="sa",Title="Excessive Sleepiness",
-                    Question = "Sitting and watching TV?",
-                    Choices = new List<string> { "Never", "slight", "moderate", "high","Please Select"},
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "Sitting inactive in a public space",
+                    Choices = new List<string> {"Never", "slight","moderate","high","Please Select"},
                     AnswerIndex = 1,
                     Score = 3
                 },
-
-                   new HypnosQuestion
+                                  new HypnosQuestion
                 {
                     Qno=8,
-                    Category="sa",Title="Excessive Sleepiness",
-                    Question = "Sitting inactive in a public space?",
-                    Choices = new List<string> { "Never", "slight", "moderate", "high","Please Select"},
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "As a passenger in a car for one hour without a break",
+                    Choices = new List<string> {"Never", "slight", "moderate", "high","Please Select"},
                     AnswerIndex = 1,
                     Score = 3
-
                 },
-                   new HypnosQuestion
+                                      new HypnosQuestion
                 {
                     Qno=9,
-                    Category="sa",Title="Excessive Sleepiness",
-                    Question = "As a passenger in a car for one hour without a break?",
-                    Choices = new List<string> { "Never", "slight", "moderate", "high","Please Select"},
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "Lying down to rest in the afternoon ",
+                    Choices = new List<string> {"Never", "slight", "moderate", "high","Please Select"},
                     AnswerIndex = 1,
                     Score = 3
                 },
-                   new HypnosQuestion
+                                          new HypnosQuestion
                 {
                     Qno=10,
-                    Category="sa",Title="Excessive Sleepiness",
-                    Question = "Lying down to rest in the afternoon ?",
-                    Choices = new List<string> { "Never", "slight", "moderate", "high","Please Select"},
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "Sitting and talking to someone",
+                    Choices = new List<string> {"Never", "slight", "moderate", "high","Please Select"},
                     AnswerIndex = 1,
                     Score = 3
-                },
-                   new HypnosQuestion
+                                          },
+                        new HypnosQuestion
                 {
                     Qno=11,
-                    Category="sa",Title="Excessive Sleepiness",
-                    Question = "Sitting and talking to someone?",
-                    Choices = new List<string> { "Never", "slight", "moderate", "high","Please Select"},
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "Sitting quietfly after lunch without alcohol",
+                    Choices = new List<string> {"Never", "slight", "moderate", "high","Please Select"},
                     AnswerIndex = 1,
                     Score = 3
                 },
-                   new HypnosQuestion
+                 new HypnosQuestion
                 {
                     Qno=12,
-                    Category="sa",Title=Excessive Sleepiness",
-                    Question = "Sitting quietfly after lunch without alcohol?",
-                    Choices = new List<string> { "Never", "slight", "moderate", "high","Please Select"},
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "In a care, while stopped for a few minutes in traffic",
+                    Choices = new List<string> {"Never", "slight", "moderate", "high","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+ new HypnosQuestion
+                {
+                    Qno=13,
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "How many times do you typically wake up during the nighttime ",
+                    Choices = new List<string> {"0", "1-3",">3","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+ new HypnosQuestion
+                {
+                    Qno=14,
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "What position do you believe you spend most of your sleep time in ",
+                    Choices = new List<string> {"Back", "side","stomach","recliner","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+ new HypnosQuestion
+                {
+                    Qno=15,
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "Do you often wake up with a dry mouth or morning headache",
+                    Choices = new List<string> {"Yes", "No","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+ new HypnosQuestion
+                {
+                    Qno=16,
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "Do you often take scheduled or unscheduled naps",
+                    Choices = new List<string> {"Yes", "No","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+ new HypnosQuestion
+                {
+                    Qno=17,
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "Have you gained weight in last 3-5 years",
+                    Choices = new List<string> {"Yes", "No","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+  new HypnosQuestion
+                {
+                    Qno=18,
+                    Category="sa",Title="Sleep_Apnea",
+                    Question = "Is there anything else you would like to tell us about your sleep",
+                    Choices = new List<string> {"100", "<100","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+
+
+
+  /*       Legs Movement */
+
+      new HypnosQuestion
+                {
+                    Qno=1,
+                    Category="Le",Title="Leg",
+                    Question = "Do you experience an uncomfortable urge to move your legs or feet, typically accompanied by unpleasant sensations ",
+                    Choices = new List<string> {"Yes", "No","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+        new HypnosQuestion
+                {
+                    Qno=20,
+                    Category="Le",Title="Leg",
+                    Question = "Is this urge or uncomfortable feeling made worse by periods of inactivty or rest",
+                    Choices = new List<string> {"Yes", "No","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+  new HypnosQuestion
+                {
+                    Qno=21,
+                    Category="Le",Title="Leg",
+                    Question = "Is this urge or sensation reliveved by walking or by moving your legs",
+                    Choices = new List<string> {"Yes", "No","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+  new HypnosQuestion
+                {
+                    Qno=22,
+                    Category="Le",Title="Leg",
+                    Question = "Do these symptoms only occur, or at least mostly occur, during the evening or nighttime hours",
+                    Choices = new List<string> {"Yes", "No","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+  new HypnosQuestion
+                {
+                    Qno=23,
+                    Category="Le",Title="Leg",
+                    Question = "Do you have any of the following ",
+                    Choices = new List<string> { "Fibromyalgia", "chronic muscle pains", "swelling in your legs", "abnormal blood flow to your legs", "leg cramps", "chronic foot tapping", "history of anemia", "Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+  new HypnosQuestion
+                {
+                    Qno=24,
+                    Category="Le",Title="Leg",
+                    Question = "Has this occurred on at least 5 total occasions over the course of the last year ",
+                    Choices = new List<string> {"Yes", "No","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+    new HypnosQuestion
+                {
+                    Qno=25,
+                    Category="Le",Title="Leg",
+                    Question = "When these symptoms occur, do they typically occur >/= 2 times per week, or <2 times per week",
+                    Choices = new List<string> { ">/=2", "<2","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+
+      /*       Sleepniess */
+
+                new HypnosQuestion
+                {
+                    Qno=26,
+                    Category="SL",Title="Sleepiness",
+                    Question = "How long have you worked in your current position?",
+                    Choices = new List<string> { "(0-3 months", "3-6 months", "6-12 months", "> 12 months","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+                  new HypnosQuestion
+                {
+                    Qno=27,
+                    Category="SL",Title="Sleepiness",
+                    Question = "What types of shifts do you typically work?",
+                    Choices = new List<string> {"1st Shift", "2nd Shift", "Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+                    new HypnosQuestion
+                {
+                    Qno=28,
+                    Category="SL",Title="Sleepiness",
+                    Question = "How many hours of sleep do you typically get per night?",
+                    Choices = new List<string> {"Yes", "No","Please Select"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+                      new HypnosQuestion
+                {
+                    Qno=29,
+                    Category="SL",Title="Sleepiness",
+                    Question = "Are you often exposed to bright light leading up to bed time?",
+                    Choices = new List<string> {"Yes", "No","Please Select"},
                     AnswerIndex = 1,
                     Score = 3
                 },
                         new HypnosQuestion
                 {
-                    Qno=13,
-                    Category="sa",
-                            Title="Excessive Sleepiness",
-                    Question = "In a care, while stopped for a few minutes in traffic?",
-                    Choices = new List<string> { "Never", "slight", "moderate", "high","Please Select"},
+                    Qno=30,
+                    Category="SL",Title="Sleepiness",
+                    Question = "Is your sleep environement generally dark, cool and quiet?",
+                    Choices = new List<string> {"<1", "1-2","2-4",">4","Please Select"},
                     AnswerIndex = 1,
                     Score = 3
                 },
-                             new HypnosQuestion
+                         new HypnosQuestion
                 {
-                    Qno=14,
-                    Category="sa",Title="Excessive Sleepiness",
-                    Question = "How many times do you typically wake up during the nighttime?",
-                    Choices = new List<string> { "0", "1-3", ">3","Please Select"},
+                    Qno=30,
+                    Category="SL",Title="Tobacco",
+                    Question = "Is your sleep environement generally dark, cool and quiet?",
+                    Choices = new List<string> {"freetext"},
                     AnswerIndex = 1,
                     Score = 3
                 },
+                         new HypnosQuestion
+                {
+                    Qno=30,
+                    Category="SL",Title="Tobacco",
+                    Question = "What time of day do you typically smoke your first cigarette",
+                    Choices = new List<string> {"freetext"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+
             };
 
-            legQuesions = new List<HypnosQuestion>()
-            {
-                                  new HypnosQuestion
-                {
-                    Qno=15,
-                    Category="sa",Title="Leg Movements",
-                    Question = "What position do you believe you spend most of your sleep time in ?",
-                    Choices = new List<string> { "Back,", "side", "stomach", "recliner","Please Select"},
-                    AnswerIndex = 1,
-                    Score = 3
-                },
-                                       new HypnosQuestion
-                {
-                    Qno=16,
-                    Category="sa",Title="Leg Movements",
-                    Question = "Do you often wake up with a dry mouth or morning headache?",
-                    Choices = new List<string> {"Yes", "No","Please Select"},
-                    AnswerIndex = 1,
-                    Score = 3
-                },
-                                            new HypnosQuestion
-                {
-                    Qno=17,
-                    Category="sa",Title="Leg Movements",
-                    Question = "Do you often take scheduled or unscheduled naps?",
-                    Choices = new List<string> {"Yes", "No","Please Select"},
-                    AnswerIndex = 1,
-                    Score = 3
-                },
-                                            new HypnosQuestion
-                {
-                    Qno=18,
-                    Category="sa",Title="Leg Movements",
-                    Question = "Have you gained weight in last 3-5 years?",
-                    Choices = new List<string> {"Yes", "No","Please Select"},
-                    AnswerIndex = 1,
-                    Score = 3
-                }
-            };
+
         }
 
 
-        public Task<List<HypnosQuestion>?> GetsleepQuestions()
+        public Task<List<HypnosQuestion>?> GetQuestions()
         {
 
             return Task.FromResult(Quesions);
         }
 
-        public Task<List<HypnosQuestion>?> GetlegQuestions()
-        {
-            //foreach (var emp in Quesions)
-            //{
-            //    if (emp.Title == cat)
-            //    {
-            //        //newQuesions = emp.Question;
-            //        newQuesions = Quesions;
-            //    }
-            //}
-            return Task.FromResult(legQuesions);
-        }
 
-        public Task<List<HypnosQuestion>?> GettobaccoQuestions()
-        {
-
-            return Task.FromResult(TobaccoQuesions);
-        }
-
-        public Task<List<HypnosQuestion>?> GetsleepnessQuestions()
-        {
-
-            return Task.FromResult(sleepnessQuesions);
-        }
     }
 }
