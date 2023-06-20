a32e0e6530d397dd5708de20db348933153a2632
Muhammad Awab <98603853+Muhammad-Awab@users.noreply.github.com>
2023-06-20 14:14:01 +0500

    Update Program.cs

--- Hypnos/Program.cs
+++ Hypnos/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<HypnosService>();
-builder.WebHost.UseStaticWebAssets()
+builder.WebHost.UseStaticWebAssets();
 builder.WebHost.UseUrls("http://0.0.0.0:5000 ");          
 var app = builder.Build();
 
